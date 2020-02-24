933a222388c886443e9843065ef4ade2b623232c
Pavel <paul88aleshin82@yandex.ru>
2020-02-24 18:07:22 +0300

    [pteamFeed] - start project. add cartfile.

---
+++ swift/pteamFeed/pteamFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pteamFeed
+//
+//  Created by Павел Алешин on 24/02/2020.
+//  Copyright © 2020 Павел Алешин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

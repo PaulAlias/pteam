451261c59edcb08ceb82eee02f476ee111096c9a
Pavel <paul88aleshin82@yandex.ru>
2020-06-02 21:50:42 +0300

    [MyPlaces] create CustomTableViewCell

---
+++ swift/MyPlaces/MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Павел Алешин on 02.06.2020.
+//  Copyright © 2020 Павел Алешин. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    
+    
+}
